560d80a1f79e37746350c48ef93fdedf91cbe264
Michael Moghaddam <6424916+mmoghaddam385@users.noreply.github.com>
2021-05-20 17:39:27 -0400

    test for SEC style input:

--- decode_test.go
+++ decode_test.go
@@ -23,26 +23,36 @@ func TestDecodeToBytes(t *testing.T) {
 		require.NoError(t, err)
 		assert.EqualValues(t, "Cat", string(results))
 	})
+}
 
+func TestDecodeFiles(t *testing.T) {
 	t.Run("BSD/Alternate style encoding", func(t *testing.T) {
 		// polygon.uu was uuencoded using the `uuencode` utility on macOS
-		encodedFile, err := os.Open("test_data/polygon.uu")
-		require.NoError(t, err)
-
-		defer encodedFile.Close()
-
-		decoder := NewDecoder(AlternateCharset)
-		decodedBytes, err := decoder.DecodeToBytes(encodedFile)
-		require.NoError(t, err)
-
-		expectedFile, err := os.Open("test_data/polygon.jpg")
-		require.NoError(t, err)
-
-		defer expectedFile.Close()
-
-		expectedBytes, err := io.ReadAll(expectedFile)
-		require.NoError(t, err)
-
-		assert.Equal(t, expectedBytes, decodedBytes)
+		decodeFile(t, NewDecoder(AlternateCharset), "test_data/polygon.uu", "test_data/polygon.jpg")
 	})
+
+	t.Run("standard/SEC style encoding", func(t *testing.T) {
+		// polygon.sec.uu was encoded in the same style that the SEC uses when disseminating binary files in filings
+		decodeFile(t, NewStandardDecoder(), "test_data/polygon.sec.uu", "test_data/polygon.jpg")
+	})
+}
+
+func decodeFile(t *testing.T, decoder Decoder, encodedFilename, decodedFilename string) {
+	encodedFile, err := os.Open(encodedFilename)
+	require.NoError(t, err)
+
+	defer encodedFile.Close()
+
+	decodedBytes, err := decoder.DecodeToBytes(encodedFile)
+	require.NoError(t, err)
+
+	expectedFile, err := os.Open(decodedFilename)
+	require.NoError(t, err)
+
+	defer expectedFile.Close()
+
+	expectedBytes, err := io.ReadAll(expectedFile)
+	require.NoError(t, err)
+
+	assert.Equal(t, expectedBytes, decodedBytes)
 }
